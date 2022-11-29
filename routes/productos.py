26a8b4fc7a0d865be85460d0777a3ada8c6c63c9
EG <erick.g.amaris@gmail.com>
2022-11-29 12:47:45 -0500

    api crud completa

--- routes/productos.py
+++ routes/productos.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 from config.db import db
 from schemas.productos import productosEntity, productoEntity
+from bson import ObjectId
+from models.productos import Producto
+
 
 productos = APIRouter()
 
@@ -10,16 +13,20 @@ def todos_los_productos():
 
 @productos.get("/productos/{id}")
 def un_producto(id: str):
-	return "un producto"
+	return productoEntity( db.productos.find_one({"_id": ObjectId(id)}) )
 
 @productos.post("/productos")
-def agregar_producto():
-	return "Agregando producto"
+def agregar_producto( producto: Producto ):
+	id = db.productos.insert_one( dict( producto ) ).inserted_id
+	return productoEntity( db.productos.find_one({"_id": ObjectId(id)}) )
 
 @productos.delete("/productos/{id}")
 def eliminar_producto(id: str):
-	return "eliminar_producto"
+	db.productos.find_one_and_delete( {"_id": ObjectId(id)} ) 
+	return "Se ha eliminado correctamente"
+
 
 @productos.put("/productos/{id}")
-def actualizar_producto(id: str):
-	return "actualizar_producto"
+def actualizar_producto(id: str, producto:Producto):
+	db.productos.find_one_and_update({"_id": ObjectId(id)}, {"$set": dict( producto ) })
+	return productoEntity( db.productos.find_one({"_id": ObjectId(id)}) )
