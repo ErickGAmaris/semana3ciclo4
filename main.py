20c7e548c6a4a4ea910489f3448c9caf50a5e56e
EG <erick.g.amaris@gmail.com>
2022-11-29 12:57:36 -0500

    Documentacion fastapi

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from routes.productos import productos
 app = FastAPI(
 		title="Aplicacion FastAPI con MongoDB",
 		description="Esta es una aplicacion donde se va hacer una API REST",
-		version="0.0.0"
+		version="0.0.1"
 	)
 
 app.include_router(productos)
--- routes/productos.py
+++ routes/productos.py
@@ -7,26 +7,36 @@ from models.productos import Producto
 
 productos = APIRouter()
 
-@productos.get("/productos")
+@productos.get("/productos",
+	tags=["productos"]
+
+)
 def todos_los_productos():
 	return productosEntity( db.productos.find() )
 
-@productos.get("/productos/{id}")
+@productos.get("/productos/{id}",
+	tags=["productos"],
+	response_model=Producto)
 def un_producto(id: str):
 	return productoEntity( db.productos.find_one({"_id": ObjectId(id)}) )
 
-@productos.post("/productos")
+@productos.post("/productos",
+	tags=["productos"],
+	response_model=Producto
+	)
 def agregar_producto( producto: Producto ):
 	id = db.productos.insert_one( dict( producto ) ).inserted_id
 	return productoEntity( db.productos.find_one({"_id": ObjectId(id)}) )
 
-@productos.delete("/productos/{id}")
+@productos.delete("/productos/{id}",
+	tags=["productos"])
 def eliminar_producto(id: str):
 	db.productos.find_one_and_delete( {"_id": ObjectId(id)} ) 
 	return "Se ha eliminado correctamente"
 
 
-@productos.put("/productos/{id}")
+@productos.put("/productos/{id}",
+	tags=["productos"])
 def actualizar_producto(id: str, producto:Producto):
 	db.productos.find_one_and_update({"_id": ObjectId(id)}, {"$set": dict( producto ) })
 	return productoEntity( db.productos.find_one({"_id": ObjectId(id)}) )
